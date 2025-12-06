4898d4eaab3cf2164721842ebf8fc1e373614f1c
Squmon <squmon074@gmail.com>
2025-12-06 14:45:19 +0500

    сделал другую имплементацию alpha синапса

--- scripts/nj/neurosci.py
+++ scripts/nj/neurosci.py
@@ -125,6 +125,36 @@ def get_alpha_synapce_pipeline(pre_synaptic, post_synaptic, tau, E_rev, G_max, V
         return state, ds_dt
     return pipeline
 
+def get_alpha_synapce_only_ds_dt_pipeline(pre_synaptic, post_synaptic, tau, E_rev, G_max, V_m, C, alpha_syn_detector_treshold, synaptic_weights, dt, treshold_interval = 0.01, *args, **kwargs):
+    @jax.jit
+    def u(alpha):
+        da_dt = jnp.empty_like(alpha)
+        da_dt.at[:, 0].set(-alpha[:, 0]/tau)
+        da_dt.at[:, 1].set((alpha[:, 0] - alpha[:, 1])/tau)
+        return da_dt
+    
+    @jax.jit
+    def I(alpha, V_m):
+        return G_max*alpha[:, 1]*(V_m - E_rev)
+    
+    @jax.jit
+    def pipeline(state, ds_dt):
+        ds_dt['alpha'] += u(state['alpha'])
+        cin = C.at[post_synaptic[:, 1]].get()
+        vim = V_m.at[post_synaptic[:, 1]].get()
+        ain = state['alpha'].at[post_synaptic[:, 0]].get()
+        ds_dt['V'] = ds_dt['V'].at[post_synaptic[:, 1]].add(-I(ain, vim)/cin)
+
+        # instant changes
+        v_ = state['V'].at[pre_synaptic[:, 0]].get()
+        dv_dt_ = ds_dt['V'].at[pre_synaptic[:, 0]].get()
+
+        delta_x = (jnp.abs(v_ - alpha_syn_detector_treshold) < treshold_interval) * (dv_dt_ > 0.0) * synaptic_weights
+        #state['alpha'] = state['alpha'].at[pre_synaptic[:, 1], 1].add(delta_x)
+        ds_dt['alpha'] = ds_dt['alpha'].at[pre_synaptic[:, 1], 1].add(delta_x/dt)
+        return state, ds_dt
+    return pipeline
+
 def get_HH_pipeline(C, ENa, EK, EL, gNa, gK, gL, *args, **kwargs):
     q = generate_hh_channels_functions(C, ENa, EK, EL, gNa, gK, gL)
     dv = q['V_dynamic']
